f4d253d0f37add3afadf807abc5656a89be89d80
Mazen ALkhadra <mazen.alkhadra@outlook.com>
2023-05-31 17:30:35 +0400

    Init

---
+++ index.js
@@ -0,0 +1,100 @@
+const http      = require('http');
+const httpProxy = require('http-proxy');
+const proxy     = httpProxy.createProxyServer({});
+const fs = require('fs');
+
+proxy.on('proxyRes', require('./score-api-handler'));
+
+http.createServer(function(req, res) {
+
+  let url = req.url;
+  let selfHandleResponse = false;
+  let target = 'http://77577.live';
+  let bbsIndx = url.indexOf('/bbs')
+  let localFIndx = url.indexOf('/ffapi')
+  let app8Index = url.indexOf('/app8');
+  let xnIndex = url.indexOf('/xn');
+  let challengSportIndex = url.indexOf('/challenges-sport');
+  let sportScoreIndx = url.indexOf('/api/v1/sportscore');
+
+  if(url == '/api/sc/player/details') {   
+    req.on("data", data => {
+      data = data.toString();
+      try {data = JSON.parse(data);} catch(er) {};
+      if(!data || !data.slug) {
+        res.writeHead(404).end();
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(fs.readFileSync(`${__dirname}/resources/data/player-details.json`));
+    });    
+    return;
+  }
+  
+  else if(url == '/api/sc/players') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(fs.readFileSync(`${__dirname}/resources/data/players.json`));
+    return;
+  }
+
+  else if(bbsIndx > -1) {
+    url = url.substring(0, bbsIndx) + url.substring(bbsIndx + 4);
+    target = 'http://bbs.77577.live';
+    req.url = url;
+  }
+
+  else if(localFIndx > -1) {
+    target = 'http://199.241.1.88'
+    req.url += '.php';
+  }
+
+  else if(app8Index > -1) {
+    url = url.substring(0, app8Index) + url.substring(app8Index + 5) + '.php';
+    target = 'http://app.8com.cloud';
+    req.url = url;
+  }
+
+  else if(xnIndex > -1) {
+    url = url.substring(0, xnIndex) + url.substring(xnIndex + 3);
+    target = 'https://xn--8868-3m6f282x.com';
+    req.url = url;
+  }
+
+  else if(challengSportIndex > -1) {
+    url = url.substring(0, challengSportIndex) + url.substring(challengSportIndex + 17);
+    target = 'http://localhost:5053';
+    req.url = url + '.php';
+  }
+
+  else if(url && url.substring(url.length - 2) === 'ph') {
+    req.url += 'p';
+    target = 'http://app.8com.cloud';
+  }
+
+  else if(url && url.substring(url.length - 3) === '.tj') {
+    req.url = req.url.replace('.tj', '.php');
+    target = 'https://tj.77577.com';
+  }
+
+  else if(url && url.substring(url.length - 3) === '.c8') {
+    req.url = req.url.replace('.c8', '.php');
+    target = 'https://c.886811.fun';
+  }
+
+  console.log(target + req.url);
+  
+  if(sportScoreIndx > -1) {
+    selfHandleResponse = true;
+    req.body = [];
+    req.on("data", data => {req.body.push(data);});
+  }
+  
+  proxy.web(req, res, { 
+    target,
+    changeOrigin: true,
+    followRedirects: true,
+    selfHandleResponse
+  });
+}).listen(5051, () => {
+  console.log("Waiting for requests...");
+});
---
+++ score-api-handler.js
@@ -0,0 +1,96 @@
+module.exports = function (proxyRes, req, res) {
+    let sportScoreIndx = req.url.indexOf('/api/v1/sportscore');
+    let sportScorematchIndx = req.url.indexOf('/data/match');
+  
+    if( sportScoreIndx == -1)
+      return;
+
+    console.log("SPORT SCORE HANDLER");
+
+    let body = [];
+    let reqBody = req.body ? JSON.parse(Buffer.concat(req.body).toString()) : {};
+    
+    proxyRes.on("data", data => body.push(data));
+        
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    proxyRes.on('end', () => { 
+      if(sportScorematchIndx > -1 && !reqBody.slug)
+        res.end(JSON.stringify(getMatchList(reqBody)));
+      else 
+        res.end(Buffer.concat(body).toString());
+    });
+  };
+
+
+  function getMatchList({leagues, teams}) {
+    let chkLeagues = Array.isArray(leagues) && leagues.length;
+    let chkTeams = Array.isArray(teams) && teams.length;
+
+    let matches = require('./resources/data/sport-score-matches.json');
+
+    if(chkLeagues || chkTeams) { 
+      matches = matches.filter(match => {
+        let valid = true;
+        if(chkLeagues)
+          valid &= leagues.includes(match.league_slug);
+        if(chkTeams) {
+          let innerMatches = [];
+          match.matches.forEach(innerMatch => {
+            if(teams.includes(innerMatch.home_team) || teams.includes(innerMatch.away_team))
+              innerMatches.push(innerMatch)
+          });
+          match.matches = innerMatches;
+          valid &= innerMatches.length > 0;
+        }
+        return valid;
+      });
+    }
+    
+    return matches;
+  }
+
+
+  async function syncAllLeagues() {
+    let axios = require('axios');
+    let leagues = [];
+    let lastPage = 1;
+    let totalPages = 2;
+
+    for(lastPage; lastPage <= totalPages; ++lastPage)
+      await axios.post("https://app.8com.cloud/api/v1/sportscore/data/league.php", {
+        "lang": "zh",
+        "page": lastPage
+      }).then(res => {
+        leagues.push(...res.data.data);
+        totalPages = Math.ceil(+res.data.total / +res.data.per_page);
+        console.log(lastPage + " out of " + totalPages);
+      }).catch(err => console.log(err));
+  
+    require('fs').writeFileSync("./resources/data/sport-score-leagues.json", JSON.stringify(leagues));
+  }
+
+  async function syncMatches() {
+    let axios = require('axios');
+    let matches = [];
+    let daysOffset = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7];
+    let now = Date.now();
+
+    for(offset of daysOffset) {
+      let date = new Date(now + (offset * 24 * 60 * 60 * 1000));
+      let day = `${date.getFullYear()}-` 
+      day += `0${date.getMonth() + 1}`.slice(-2);
+      day += '-' + `0${date.getDate()}`.slice(-2);
+
+      await axios.post("https://app.8com.cloud/api/v1/sportscore/data/match.php", {
+        "lang":"en",
+        "date": day,
+        "sport":"football",
+        "timezone":"+08:00"
+      }).then(res => {
+        matches.push(...res.data.data);
+      }).catch(err => console.log(err));
+    }
+    require('fs').writeFileSync("./resources/data/sport-score-matches.json", JSON.stringify(matches));
+  }
+
